56b86e06f5a185cc8ec0ed10010db26c4b8d37e8
grapiuna <andrepinto1981@gmail.com>
2024-02-23 11:08:54 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,6 @@
 
 async function fetchProfileData() {
-    const url = "https://raw.githubusercontent.com/grapiuna/js-developer-portfolio/main/data/profile.json"
-    const response = await fetch(url)
-    const profileData = await response.json()
-    return profileData
+    const url = "https://raw.githubusercontent.com/grapiuna/js-developer-portfolio/main/data/profile.json";
+    const profileData = await fetch(url);
+    return await profileData.json();
 }
